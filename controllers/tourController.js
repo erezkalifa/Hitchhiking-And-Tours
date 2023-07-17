b95d572a2822c191bf475f82c8bf34bbfa38c745
erezkalifa <erezkalifa10@gmail.com>
2023-07-17 15:26:44 +0300

    First commit of the back-end of project

---
+++ controllers/tourController.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'The request does not contain properties name or price',
+    });
+  }
+  next();
+};
+
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour ID is: ${val}`);
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'invald Id' });
+  }
+
+  next();
+};
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    requstedAt: req.requestedTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({ status: 'success', data: { tour } });
+};
+
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Tour updated>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        // Handle the error
+        console.log('Error writing to file:', err);
+        return;
+      }
+      res.status(201).json({
+        status: 'success',
+        data: newTour,
+      });
+    }
+  );
+};
