62af90be80bff047eb3fa610f217db530b22ff42
erezkalifa <erezkalifa10@gmail.com>
2023-07-19 12:27:52 +0300

    Refactoring the database code to MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,5 @@
 const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const Tour = require('./../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || req.body.price) {
@@ -14,30 +11,20 @@ exports.checkBody = (req, res, next) => {
   next();
 };
 
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour ID is: ${val}`);
-  const id = req.params.id * 1;
-  if (id > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'invald Id' });
-  }
-
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requstedAt: req.requestedTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+  // const id = req.params.id * 1;
+  // const tour = tours.find((el) => el.id === id);
 
   res.status(200).json({ status: 'success', data: { tour } });
 };
@@ -59,24 +46,29 @@ exports.deleteTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  // const newId = tours[tours.length - 1].id + 1;
+  // const newTour = Object.assign({ id: newId }, req.body);
 
-  tours.push(newTour);
+  // tours.push(newTour);
 
-  fs.writeFile(
-    './dev-data/data/tours-simple.json',
-    JSON.stringify(tours),
-    (err) => {
-      if (err) {
-        // Handle the error
-        console.log('Error writing to file:', err);
-        return;
-      }
-      res.status(201).json({
-        status: 'success',
-        data: newTour,
-      });
-    }
-  );
+  // fs.writeFile(
+  //   './dev-data/data/tours-simple.json',
+  //   JSON.stringify(tours),
+  //   (err) => {
+  //     if (err) {
+  //       // Handle the error
+  //       console.log('Error writing to file:', err);
+  //       return;
+  //     }
+  //     res.status(201).json({
+  //       status: 'success',
+  //       data: newTour,
+  //     });
+  //   }
+  // );
+
+  res.status(201).json({
+    status: 'success',
+    data: newTour,
+  });
 };
